3d144bfb3c126c2b77333f98035248d99aa17e01
ahussain5253 <41170583+ahussain5253@users.noreply.github.com>
2019-10-11 19:01:33 -0400

    Wage Calculator

---
+++ WagesCalculator/WagesCalculator/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	  
+	int annualWage;
+	int monthlyWage;
+	int dailyWage;
+	int hourlyWage;
+	int minuteWage;
+	int identifier;
+
+	cout << endl;
+	cout << "Enter what kind of wage you are entering:" << endl << endl << "(a for annualy, m for monthly, d for daily, and h for hourly" << endl;
+	cin >> identifier;
+
+	if (identifier == 'a') {
+
+
+	}
+	else if (identifier == 'm') {
+
+
+	}
+	else if (identifier == 'd') {
+
+
+	}
+	else if (identifier == 'h') {
+
+
+	}
+	else {
+
+		cout << endl;
+		cout << "Error: Please type in either an a, m, d, or h an h for an output." << endl << "Terminating program...";
+		return 0;
+
+	}
+	
+
+}
