c5e677c8abf5ef7af7a1ebbafd3aa77fa930d8e7
Ammar Hussain <ammarh786@gmail.com>
2021-11-27 16:02:40 -0500

    ch

---
+++ C++ Testing/C++ Testing/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+
+	string hex = "2000000000";
+
+	unsigned long long = stoull(hex);
+
+	cout << num;
+
+
+	/*
+	vector<char> dec;
+
+	for (int i = 0; i < hex.length(); i++) {
+
+		dec.push_back(hex.at(i));
+
+	}
+
+	for (int i = 0; i < dec.size(); i++) {
+
+		cout << dec.at(i);
+
+	}
+
+	*/
+
+}
