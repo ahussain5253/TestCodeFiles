8dfad147a4746136e778f88052a7b5769110cb56
Ammar Hussain <ammarh786@gmail.com>
2022-01-29 15:09:05 -0500

    h

--- C++ Testing/C++ Testing/Source.cpp
+++ C++ Testing/C++ Testing/Source.cpp
@@ -6,49 +6,20 @@
 #include <string>
 using namespace std;
 
-vector<string> doSomething(vector<string> A) {
 
-	for (int i = A.size() - 1; i >= 0; i--) {
 
-		bool wasFound = false;
 
-		for (int j = i - 1; j >= 0; j--) {
 
-			if (A.at(i) == A.at(j)) {
 
-				A.erase(A.begin() + j);
-				wasFound = true;
-				i--;
+int main() {
 
-			}
 
-		}
-		if (!wasFound) {
 
-			A.erase(A.begin() + i);
 
-		}
 
-	}
 
-	return A;
+
 }
-
-	int main() {
-
-		vector<string> animals = { "giraffe", "cat", "bat", "dog", "dog", "cat", "rat", "giraffe", "giraffe", " snake" };
-
-		animals.push
-
-		doSomething(animals);
-
-		for (int i = 0; i < animals.size(); i++) {
-
-			cout << animals.at(i) << " ";
-
-		}
-
-	}
 		 
 
 
