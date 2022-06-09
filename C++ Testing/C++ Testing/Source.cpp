f932c7185e43cdf465e9b83bc1815a0970a9a74d
Ammar Hussain <ammarh786@gmail.com>
2022-06-08 22:37:27 -0400

    h

--- C++ Testing/C++ Testing/Source.cpp
+++ C++ Testing/C++ Testing/Source.cpp
@@ -1,25 +1,43 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
+
 #include <iostream>
-#include <vector>
-#include <string>
+
 using namespace std;
 
+bool isPow2(int num) {
 
+    double final;
 
+    final = num;
 
+    while (final != 1) {
 
+        final /= 2;
+
+        if (final < 1) {
+
+            break;
+
+        }
+
+    }
+
+    if (final == 1) {
+
+        return true;
+
+    }
+    else {
+
+        return false;
+    }
+
+}
 
 int main() {
 
+    cout << "Is 14 a power of 2?" << " " << isPow2(14) << endl;
 
 
 
-
-
-
+    return 0;
 }
-		 
-
-
